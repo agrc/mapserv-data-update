8fc3330c90d094b1e4165888b6b63294db5330cf
Scott Davis <stdavis@utah.gov>
2015-12-04 08:43:12 -0700

    better agrc version and valid email format

--- settings/common.py
+++ settings/common.py
@@ -1,5 +1,2 @@
-NOTIFICATION_EMAILS = ['stdavis@utah.gov']
-LOCATOR_NOTIFICATION_EMAILS = ['stdavis@utah.gov',
-                               'kkgreen@utah.gov',
-                               'zbeck@utah.gov',
-                               'sgourley@utah.gov']
+NOTIFICATION_EMAILS = 'stdavis@utah.gov'
+LOCATOR_NOTIFICATION_EMAILS = 'stdavis@utah.gov;kkgreen@utah.gov;zbeck@utah.gov;sgourley@utah.gov'
